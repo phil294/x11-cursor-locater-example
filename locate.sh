a9ea4e9aa80eb18e03aa175edc0605f772d2bae3
Philip Waritschlager <eochgls@web.de>
2018-04-25 00:48:56 +0200

    Update locate.sh

--- locate.sh
+++ locate.sh
@@ -6,9 +6,9 @@ posy=$(cut -d ' ' -f 2 <<< "$loc")
 x=$(cut -d : -f 2 <<< "$posx")
 y=$(cut -d : -f 2 <<< "$posy")
 
-tkbash highlightcursor window --tooltip -x "$((x-20))" -y "$((y-20))" -w 41 -h 41
+tkbash highlightcursor --tooltip -x "$((x-20))" -y "$((y-20))" -w 41 -h 41
 for c in red green blue yellow black white pink orange; do
-	tkbash highlightcursor --tk ".w configure -background $c"
+	tkbash highlightcursor --background "$c"
 	sleep 0.03
 done
-tkbash highlightcursor window --close
+tkbash highlightcursor --close
